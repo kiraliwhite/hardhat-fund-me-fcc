e0e4dfa093958ff10c186dfc44a71a100678c432
kiraliwhite <104891984+kiraliwhite@users.noreply.github.com>
2022-10-13 15:58:52 +0800

    Update FundMe.test.js

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,147 +1,178 @@
-const { assert, expect } = require("chai")
-const { network, deployments, ethers } = require("hardhat")
-const { developmentChains } = require("../../helper-hardhat-config")
+const { ethers, deployments, getNamedAccounts } = require("hardhat");
+const { assert, expect } = require("chai");
 
-!developmentChains.includes(network.name)
-    ? describe.skip
-    : describe("FundMe", function () {
-          let fundMe
-          let mockV3Aggregator
-          let deployer
-          const sendValue = ethers.utils.parseEther("1")
-          beforeEach(async () => {
-              // const accounts = await ethers.getSigners()
-              // deployer = accounts[0]
-              deployer = (await getNamedAccounts()).deployer
-              await deployments.fixture(["all"])
-              fundMe = await ethers.getContract("FundMe", deployer)
-              mockV3Aggregator = await ethers.getContract(
-                  "MockV3Aggregator",
-                  deployer
-              )
-          })
+//撰寫測試使用describe,一個箭頭匿名函式,這個是最上層的代表整個測試
+describe("FundMe", async () => {
+  let fundMe; //設定為外層全域變數,使其在底下的test也能夠讀取,是一個變數,主要的用途為存放抓取部署好的智能合約
+  let deployer; //存放帳號
+  let mockV3Aggregator;
+  //const sendValue = "1000000000000000000" // 1ETH
+  const sendValue = ethers.utils.parseEther("1"); //宣告變數1ETH 等同於上面那行
 
-          describe("constructor", function () {
-              it("sets the aggregator addresses correctly", async () => {
-                  const response = await fundMe.getPriceFeed()
-                  assert.equal(response, mockV3Aggregator.address)
-              })
-          })
+  //在進行it測試前,需要做的事情放在beforeEach內
+  beforeEach(async () => {
+    //抓取帳號deployer從getNamedAccounts而來
+    //我們將把它包裝起來，這樣我們就可以抓住這個deployer object並將其分配給聲明declare
+    //deployer這個帳號 等於 從getNamedAccounts裡面的deployer 而來,而這個section的預設帳號為0
+    //就會依照該網路的不同而抓取不同的default帳號 詳情看hardhat.config.js
+    deployer = (await getNamedAccounts()).deployer;
+    console.log(`deployer is ${deployer}`);
+    //使用deployments的fixture會掃描在deploy資料夾底下,所有tags是all的script都會執行
+    await deployments.fixture("all");
+    //部署出來的合約使用ethers.getContract去抓取,關鍵字是FundMe這個合約,帳號是deployer
+    fundMe = await ethers.getContract("FundMe", deployer);
+    //抓取部署出來的mock合約MockV3Aggregator,塞在變數內,
+    //使用deployer帳號跟合約連接(因為如果是其他帳號碰到onlyOwenr等modifier就無法與合約互動)
+    mockV3Aggregator = await ethers.getContract("MockV3Aggregator", deployer);
+  });
 
-          describe("fund", function () {
-              // https://ethereum-waffle.readthedocs.io/en/latest/matchers.html
-              // could also do assert.fail
-              it("Fails if you don't send enough ETH", async () => {
-                  await expect(fundMe.fund()).to.be.revertedWith(
-                      "You need to spend more ETH!"
-                  )
-              })
-              // we could be even more precise here by making sure exactly $50 works
-              // but this is good enough for now
-              it("Updates the amount funded data structure", async () => {
-                  await fundMe.fund({ value: sendValue })
-                  const response = await fundMe.getAddressToAmountFunded(
-                      deployer
-                  )
-                  assert.equal(response.toString(), sendValue.toString())
-              })
-              it("Adds funder to array of funders", async () => {
-                  await fundMe.fund({ value: sendValue })
-                  const response = await fundMe.getFunder(0)
-                  assert.equal(response, deployer)
-              })
-          })
-          describe("withdraw", function () {
-              beforeEach(async () => {
-                  await fundMe.fund({ value: sendValue })
-              })
-              it("withdraws ETH from a single funder", async () => {
-                  // Arrange
-                  const startingFundMeBalance =
-                      await fundMe.provider.getBalance(fundMe.address)
-                  const startingDeployerBalance =
-                      await fundMe.provider.getBalance(deployer)
+  //撰寫針對此fundMe合約的constructor的測試,不使用箭頭函式改成一般的function也可以
+  describe("constructor", async function () {
+    it("sets the aggregator addresses correctly", async () => {
+      //當合約部署完成後的priceFeed 就是等於該網路的餵價合約的地址
+      const response = await fundMe.priceFeed();
+      //使用assert比對部署出來合約的priceFeed,是否等於mockV3Aggregator的地址
+      assert.equal(response, mockV3Aggregator.address);
+    });
+  });
 
-                  // Act
-                  const transactionResponse = await fundMe.withdraw()
-                  const transactionReceipt = await transactionResponse.wait()
-                  const { gasUsed, effectiveGasPrice } = transactionReceipt
-                  const gasCost = gasUsed.mul(effectiveGasPrice)
+  describe("fund", async () => {
+    it("Fails if you don't send enough ETH", async function () {
+      //這行expect會去抓fundMe合約中的fund function的require revert關鍵字"You need to spend more ETH""
+      await expect(fundMe.fund()).to.be.reverted;
+    });
+    //撰寫fund function的mapping測試,檢查送到function內的錢,是否等於我們宣告的錢
+    it("updated the amount funded data structure", async function () {
+      //傳送1ETH到fundMe合約中的fund function
+      await fundMe.fund({ value: sendValue });
+      //呼叫fundMe合約中的mapping,使用的是deployer的地址
+      const response = await fundMe.fundMoney(deployer);
+      //檢查mapping中的價值,是否等於我們送進fund function的價值,會需要toString是因為這兩個都是bigNumber
+      assert.equal(response.toString(), sendValue.toString());
+    });
+    //撰寫fund function的array測試,檢查捐款人的陣列是否正確
+    it("Adds funder to array of funders", async function () {
+      //對fund function發送ETH
+      await fundMe.fund({ value: sendValue });
+      //提取fundMe 合約中的funders陣列index0,因為上述是捐款,所以此時記錄了捐款人的地址
+      const funder = await fundMe.funders(0);
+      //檢查陣列中index0捐款人地址是否等於deployer地址
+      assert.equal(funder, deployer);
+    });
+  });
+  describe("withdraw", async () => {
+    beforeEach(async function () {
+      await fundMe.fund({ value: sendValue });
+    });
+    it("Withdraw ETH from a single founder", async () => {
+      //Arrange
+      //取得合約中的初始餘額存在變數中
+      const startingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      );
+      //取得創建合約者deployer的初始餘額,存在變數中
+      const startingDeployerBalance = await fundMe.provider.getBalance(
+        deployer
+      );
+      //Act
+      //呼叫合約的withdraw function並等待一個區塊確認
+      const transactionResponse = await fundMe.withdraw();
+      const transactionReceipt = await transactionResponse.wait(1);
 
-                  const endingFundMeBalance = await fundMe.provider.getBalance(
-                      fundMe.address
-                  )
-                  const endingDeployerBalance =
-                      await fundMe.provider.getBalance(deployer)
+      //從transactionReceipt物件中提取兩個物件gasUsed和effectiveGasPrice
+      const { gasUsed, effectiveGasPrice } = transactionReceipt;
+      //取得gas花費,等於gasUsed乘以effectiveGasPrice,但因為兩者都是bigNumber,所以使用mul
+      const gasCost = gasUsed.mul(effectiveGasPrice);
 
-                  // Assert
-                  // Maybe clean up to understand the testing
-                  assert.equal(endingFundMeBalance, 0)
-                  assert.equal(
-                      startingFundMeBalance
-                          .add(startingDeployerBalance)
-                          .toString(),
-                      endingDeployerBalance.add(gasCost).toString()
-                  )
-              })
-              // this test is overloaded. Ideally we'd split it into multiple tests
-              // but for simplicity we left it as one
-              it("is allows us to withdraw with multiple funders", async () => {
-                  // Arrange
-                  const accounts = await ethers.getSigners()
-                  for (i = 1; i < 6; i++) {
-                      const fundMeConnectedContract = await fundMe.connect(
-                          accounts[i]
-                      )
-                      await fundMeConnectedContract.fund({ value: sendValue })
-                  }
-                  const startingFundMeBalance =
-                      await fundMe.provider.getBalance(fundMe.address)
-                  const startingDeployerBalance =
-                      await fundMe.provider.getBalance(deployer)
+      //取得呼叫withdraw function後的合約餘額
+      const endingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      );
+      //取得呼叫withdraw function後的deployer帳戶餘額
+      const endingDeployerBalance = await fundMe.provider.getBalance(deployer);
+      //assert
+      //比較,呼叫withdraw function後的合約餘額 是否為0
+      assert.equal(endingFundMeBalance, 0);
+      //比較,呼叫withdraw function後的deployer帳戶餘額加上gasCost,是否等於初始合約餘額+初始deployer帳戶餘額
+      //因為是把所有的錢,從合約中提領出來,存到deployer帳戶內,且要扣掉呼叫withdraw function的Gas Fee
+      assert.equal(
+        startingFundMeBalance.add(startingDeployerBalance).toString(),
+        endingDeployerBalance.add(gasCost).toString()
+      );
+    });
 
-                  // Act
-                  const transactionResponse = await fundMe.cheaperWithdraw()
-                  // Let's comapre gas costs :)
-                  // const transactionResponse = await fundMe.withdraw()
-                  const transactionReceipt = await transactionResponse.wait()
-                  const { gasUsed, effectiveGasPrice } = transactionReceipt
-                  const withdrawGasCost = gasUsed.mul(effectiveGasPrice)
-                  console.log(`GasCost: ${withdrawGasCost}`)
-                  console.log(`GasUsed: ${gasUsed}`)
-                  console.log(`GasPrice: ${effectiveGasPrice}`)
-                  const endingFundMeBalance = await fundMe.provider.getBalance(
-                      fundMe.address
-                  )
-                  const endingDeployerBalance =
-                      await fundMe.provider.getBalance(deployer)
-                  // Assert
-                  assert.equal(
-                      startingFundMeBalance
-                          .add(startingDeployerBalance)
-                          .toString(),
-                      endingDeployerBalance.add(withdrawGasCost).toString()
-                  )
-                  // Make a getter for storage variables
-                  await expect(fundMe.getFunder(0)).to.be.reverted
+    //撰寫withdraw有多個捐款人的測試
+    it("allows us to withdraw with mulitple funders", async () => {
+      //arrange
+      //使用ethers的library抓回所有的Signer,存到變數中,這個accounts 是一個物件陣列
+      //因為在本地端執行測試,所以這個accounts會有20個fake account存在物件陣列中
+      const accounts = await ethers.getSigners();
+      //console.log(accounts);
+      //可以列出物件陣列中的accounts[0],也就是deployer的資訊
+      //console.log(accounts[0]);
+      //寫一個for迴圈 用來遍歷整個物件陣列accounts,因為0是deployer,所以從1開始
+      for (let i = 1; i < 6; i++) {
+        //使用for迴圈的index,抓取物件陣列accounts的物件,然後將contract與該物件connect
+        const fundMeConnectedContract = await fundMe.connect(accounts[i]);
+        //呼叫合約中的fund function,此時與合約連接的不是deployer帳戶,而是accounts陣列中照順序的其他帳戶
+        //發送金額給fund function
+        await fundMeConnectedContract.fund({ value: sendValue });
+      }
+      //取得創建合約中的初始餘額,存在變數中
+      const startingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      );
+      //取得創建合約者deployer的初始餘額,存在變數中
+      const startingDeployerBalance = await fundMe.provider.getBalance(
+        deployer
+      );
+      //Act
+      //呼叫合約的withdraw function並等待一個區塊確認
+      const transactionResponse = await fundMe.withdraw();
+      const transactionReceipt = await transactionResponse.wait(1);
 
-                  for (i = 1; i < 6; i++) {
-                      assert.equal(
-                          await fundMe.getAddressToAmountFunded(
-                              accounts[i].address
-                          ),
-                          0
-                      )
-                  }
-              })
-              it("Only allows the owner to withdraw", async function () {
-                  const accounts = await ethers.getSigners()
-                  const fundMeConnectedContract = await fundMe.connect(
-                      accounts[1]
-                  )
-                  await expect(
-                      fundMeConnectedContract.withdraw()
-                  ).to.be.revertedWith("FundMe__NotOwner")
-              })
-          })
-      })
+      //從transactionReceipt物件中提取兩個物件gasUsed和effectiveGasPrice
+      const { gasUsed, effectiveGasPrice } = transactionReceipt;
+      //取得gas花費,等於gasUsed乘以effectiveGasPrice,但因為兩者都是bigNumber,所以使用mul
+      const gasCost = gasUsed.mul(effectiveGasPrice);
+
+      //取得呼叫withdraw function後的合約餘額
+      const endingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      );
+      //取得呼叫withdraw function後的deployer帳戶餘額
+      const endingDeployerBalance = await fundMe.provider.getBalance(deployer);
+
+      //assert
+      //比較,呼叫withdraw function後的合約餘額 是否為0
+      assert.equal(endingFundMeBalance, 0);
+      //比較,呼叫withdraw function後的deployer帳戶餘額加上gasCost,是否等於初始合約餘額+初始deployer帳戶餘額
+      //因為是把所有的錢,從合約中提領出來,存到deployer帳戶內,且要扣掉呼叫withdraw function的Gas Fee
+      assert.equal(
+        startingFundMeBalance.add(startingDeployerBalance).toString(),
+        endingDeployerBalance.add(gasCost).toString()
+      );
+
+      //因為當合約owner呼叫withdraw function之後,我們有寫清除funders捐款人名單,所以預期此行會出錯
+      //因為捐款人陣列已被清空
+      await expect(fundMe.funders(0)).to.be.reverted;
+
+      //使用for迴圈當index,取得捐款地址與金額的mapping,所有捐款地址的金額,都是0
+      for (let i = 1; i < 6; i++) {
+        assert.equal(await fundMe.fundMoney(accounts[i].address), 0);
+      }
+    });
+
+    //測試只有deployer帳號能夠呼叫withdraw function
+    it("Only allows the owner to withdraw", async function () {
+      //從ethers 取得accounts物件陣列
+      const accounts = await ethers.getSigners();
+      //宣告一個變數attacker,將物件陣列中的index1的帳號物件塞進去
+      //const attacker = accounts[1];
+      //使用index1的帳號連線到contract
+      const attackerConnectedContract = await fundMe.connect(accounts[1]);
+      //預期該帳號無法呼叫withdraw function且會報錯
+      await expect(attackerConnectedContract.withdraw()).to.be.reverted;
+    });
+  });
+});
